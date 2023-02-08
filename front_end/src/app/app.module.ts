35cd9644f31ff793e1fa479f81698b93963bb4cd
Tony Gupta <guptat07@gmail.com>
2023-02-07 22:29:51 -0500

    Working Input Form

--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -5,15 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatSlideToggleModule,
+    MatInputModule,
     BrowserModule,
     AppRoutingModule,
-	  HttpClientModule
+	  HttpClientModule,
+   BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
