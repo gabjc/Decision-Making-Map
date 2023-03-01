9b9fea69006529cbda84ef91dbf2ca8072fbfe1b
gabjc <gabtez23@gmail.com>
2023-02-28 21:36:26 -0500

    itinerary add

--- back-end/user.go
+++ back-end/user.go
@@ -27,6 +27,55 @@ func InitDB() {
 	db.AutoMigrate(&User{})
 }
 
+var exists = struct{}{}
+
+// a set to find the itineraries that a user holds
+type set struct {
+	m map[string]struct{}
+}
+
+func NewSet() *set {
+	s := &set{}
+	s.m = make(map[string]struct{})
+	return s
+}
+
+func (s *set) Add(value string) {
+	s.m[value] = exists
+}
+
+func (s *set) Remove(value string) {
+	delete(s.m, value)
+}
+
+func (s *set) Contains(value string) bool {
+	_, c := s.m[value]
+	return c
+}
+
+/*
+// potential code that we can use to see if a user exists at all
+func (u User) Exists(id int) bool {
+	exists := false
+	for _, user := range u {
+		if user.ID == id {
+			return true
+		}
+	}
+	return exists
+}
+
+// FindByName returns the user with the given name, or returns an error
+func (u User) FindByName(name string) (User, error) {
+	for _, user := range u {
+		if user.Name == name {
+			return user, nil
+		}
+	}
+	return User{}, errors.New("USER_NOT_FOUND")
+}
+*/
+
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
