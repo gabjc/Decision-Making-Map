c50135e4bed472595266c06ff1e131be9673f15d
Luke160 <73538537+Luke160@users.noreply.github.com>
2023-02-28 21:25:09 -0500

    Routing Complete

--- front-end/src/app/signin/signin.component.spec.ts
+++ front-end/src/app/signin/signin.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignInComponent } from './sign-in.component';
+import { signinComponent } from './signin.component';
 
-describe('SignInComponent', () => {
-  let component: SignInComponent;
-  let fixture: ComponentFixture<SignInComponent>;
+describe('signinComponent', () => {
+  let component: signinComponent;
+  let fixture: ComponentFixture<signinComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      declarations: [ signinComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SignInComponent);
+    fixture = TestBed.createComponent(signinComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
