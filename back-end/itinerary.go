884f0047ca08b150f27710f142d984012c259f3a
gabjc <gabtez23@gmail.com>
2023-03-25 23:39:08 -0400

    Developed method for multiples and created functions

--- back-end/itinerary.go
+++ back-end/itinerary.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -16,10 +18,60 @@ type votedLocation struct {
 
 type Itinerary struct {
 	//ItineraryID    string        `json:"id"`
-	Name           string         `json:"name"`
-	Address        string         `json:"address"`
-	Radius         string         `json:"radius"`
-	savedLocations map[string]int `json:"saved_locations"`
+	Name           string `json:"name"`
+	Address        string `json:"address"`
+	Radius         string `json:"radius"`
+	SavedLocations string `json:"saved_locations"`
+	locationMap    map[string]int
+}
+
+func AddSavedLocation(locale string, itin Itinerary) {
+	//TODO: parse data from the frontend, then add it to the map, discuss with frontend how they will send the data to the backend
+	var location string
+	stringLocale := strings.Fields(locale)
+	for i, word := range stringLocale {
+		if i == 0 {
+			location = word
+		}
+		/*
+			else if (i == 1) {
+
+			}
+		*/
+	}
+	_, found := itin.locationMap[location]
+	if !found {
+		itin.locationMap[location] = 0
+	}
+
+}
+
+// Used to increase or decrease the vote for a voted location, or to delete a location from the map if it is 0
+func UpdateSavedLocation(locale string, itin Itinerary, update bool) {
+	if update {
+		itin.locationMap[locale] += 1
+	}
+	if !update {
+		itin.locationMap[locale] -= 1
+	}
+}
+
+// Used to create a string so you can send it through JSON
+func CreateJsonFromMap(data map[string]int) string {
+	jsonString, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	} else {
+		return string(jsonString)
+	}
+	return ""
+}
+
+// Potentially used when a person presses a "New Itinerary" button
+func createNewItinerary() Itinerary {
+	//TODO: save this to a person
+	newItin := Itinerary{"Untitled Itinerary", "No Address", "No Radius", "", make(map[string]int)}
+	return newItin
 }
 
 // FIXME: finish implementing, add test file and functions
@@ -37,7 +89,7 @@ func PostItinerary(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
-	itinerary := Itinerary{ /*ItineraryID: params["id"],*/ Name: params["name"], Address: params["address"], Radius: params["radius"]} //, savedLocations: params["saved_locations"]
+	itinerary := Itinerary{ /*ItineraryID: params["id"],*/ Name: params["name"], Address: params["address"], Radius: params["radius"], SavedLocations: params["saved_locations"]}
 	json.NewDecoder(r.Body).Decode(&itinerary)
 	db.Create(&itinerary)
 
