ce1391d44631a34b1b8a505e29836f89b4a54f96
Joshua Lamb <joshua.lamb55@gmail.com>
2023-03-30 00:12:08 -0400

    Update status.go

--- back-end/status.go
+++ back-end/status.go
@@ -9,6 +9,7 @@ func GetStatus(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+// TODO: figure out CORS problems with frontend
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
